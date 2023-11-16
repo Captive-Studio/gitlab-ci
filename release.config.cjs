101bca7df973dcdaafacedc69ad7d78ec011c8ba
Julien Polo <julien.polo@captive.fr>
2023-11-16 14:30:44 +0100

    💚 Fix release script

--- release.config.cjs
+++ release.config.cjs
@@ -1,5 +1,3 @@
-/* eslint-disable import/no-unresolved */
-
 const makeConfig = require('@wavevision/semantic-release/config');
 const { CONFIG_GITLAB } = require('@wavevision/semantic-release/config/constants');
 
@@ -8,15 +6,23 @@ const semanticConfig = makeConfig({
   branches: ['main', 'next'],
   git: { enabled: true, assets: ['package.json', 'README.md', 'CHANGELOG.md'] },
 });
-semanticConfig.plugins.splice(
-  1, // position
-  0, // delete nothing
-  [
-    '@semantic-release/exec',
-    // eslint-disable-next-line no-template-curly-in-string
-    { prepareCmd: "sed -i -e 's/  ref: .*/  ref: ${nextRelease.version}/g' README.md" },
+const semanticConfigExt = {
+  ...semanticConfig,
+  plugins: [
+    [
+      '@semantic-release/exec',
+      // eslint-disable-next-line no-template-curly-in-string
+      { prepareCmd: "sed -i -e 's/  ref: .*/  ref: ${nextRelease.version}/g' README.md" },
+    ],
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    ...semanticConfig.plugins.filter((plugin) => plugin !== '@semantic-release/gitlab'),
+    [
+      '@semantic-release/gitlab',
+      {
+        gitlabUrl: 'https://git.captive.fr',
+      },
+    ],
   ],
-  '@semantic-release/release-notes-generator',
-  '@semantic-release/changelog'
-);
-module.exports = semanticConfig;
+};
+module.exports = semanticConfigExt;
